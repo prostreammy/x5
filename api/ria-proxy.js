9fd2063f7c68ce859faf0f0f025ed17a278b1385
prostreammy <prostream.my@gmail.com>
2025-12-18 00:02:11 +0800

    Create ria-proxy.js

---
+++ api/ria-proxy.js
@@ -0,0 +1,21 @@
+export default async function handler(req, res) {
+    const targetUrl = "https://load.perfecttv.net/mpd/ria/manifest.mpd?username=vip_r92bmh1k&password=yb3IpqrB&channel=riafhd";
+
+    try {
+        const response = await fetch(targetUrl, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36'
+            }
+        });
+
+        const data = await response.text();
+
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Content-Type', 'application/dash+xml');
+        res.setHeader('Cache-Control', 'no-store, max-age=0');
+        
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(500).send("Proxy error");
+    }
+}
